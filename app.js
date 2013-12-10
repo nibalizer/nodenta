0ee6135d0d8fc08c4cab3e2b277dc0b0041971c6
Spencer Krum <krum.spencer@gmail.com>
2013-12-09 22:22:59 -0800

    echo server

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.get('/hello.txt', function(req, res){
   res.send('Hello World');
 });
 
+app.get('/echo/:string', function(req, res){
+  res.send(req.params.string);
+});
+
 
 app.listen(3000);
 console.log('Listening on port 3000');
