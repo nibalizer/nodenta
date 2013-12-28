0ff1c9370efa84730b191c8828951087f6810c26
Spencer Krum <krum.spencer@gmail.com>
2013-12-27 23:41:41 -0800

    can now take a json blob

--- app.js
+++ app.js
@@ -40,7 +40,18 @@ app.get('/3/get', function(req, res){
     { prop = list[i] } } res.send(prop) } );
 });
 
+app.post('/api/1/get', function(req, res){
+  var filesystem = req.body.filesystem;
+  var property = req.body.property;
+  zfs.get({property: property }, function(err, list){ 
+  var prop;
+  for (var i=0; i<list.length; i++) {
+  if ( list[i].name == filesystem ) 
+    { prop = list[i] } } res.send(prop) } );
+});
 
 
 app.listen(3000);
 console.log('Listening on port 3000');
+
+
