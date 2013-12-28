3fb8741304408f0d5cc61d33ebcf244b3afaf03b
Spencer Krum <krum.spencer@gmail.com>
2013-12-27 17:42:12 -0800

    list works

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ app.get('/echo/:string', function(req, res){
   res.send(req.params.string);
 });
 
+app.get('/list', function(req, res){
+  zfs.list(function(err, list){ res.send(list) } );
+});
+
 
 app.listen(3000);
 console.log('Listening on port 3000');
