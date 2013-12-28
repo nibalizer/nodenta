92111e081161635bd711766ec612b874a888dd43
Spencer Krum <krum.spencer@gmail.com>
2013-12-27 18:16:27 -0800

    can now specify which filesystem in a header

--- app.js
+++ app.js
@@ -19,6 +19,27 @@ app.get('/get/:property', function(req, res){
   zfs.get({property: req.params.property }, function(err, list){ res.send(list) } );
 });
 
+app.get('/2/get/:property', function(req, res){
+  var filesystem = req.headers['filesystem'];
+  zfs.get({property: req.params.property }, function(err, list){ 
+
+//  var prop = list.map ( function (element) { if ( element.name == 'picombinator/nibalizer/science' ) { return element }} )
+
+
+  var prop;
+
+
+  for (var i=0; i<list.length; i++) {
+
+  if ( list[i].name == 'picombinator/nibalizer/science' ) 
+    { prop = list[i] }
+
+  } 
+
+
+  res.send(prop) } );
+});
+
 
 app.listen(3000);
 console.log('Listening on port 3000');
